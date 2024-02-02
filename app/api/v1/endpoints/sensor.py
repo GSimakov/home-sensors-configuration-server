e3f9c37188b3076ccfe6f61bfa08ccf240b39a24
gsimakov <yolo73104@gmail.com>
2024-02-02 12:00:05 +0500

    sensor api update

--- app/api/v1/endpoints/sensor.py
+++ app/api/v1/endpoints/sensor.py
@@ -1,15 +1,10 @@
-from uuid import UUID
-
-from fastapi import APIRouter, status, Depends, Query, Body, File, Response, UploadFile
+from fastapi import APIRouter, status, Depends
 from fastapi_pagination import Params
-from six import BytesIO
-from sqlmodel import and_, select, col, or_, text
-from typing import Annotated
 
 from app import schemas
 from app import models
 from app import crud
-from .. import dependencies as deps
+from app.api.v1 import dependencies as deps
 from app.utils import checks
 
 from app.schemas.response_schema import (
@@ -65,115 +60,36 @@ async def update_sensor_by_id(
         await checks.measurements_type_is_exist(id=sensor.measurement_type_id)
 
     updated = await crud.sensor.update(obj_current=current_sensor, obj_new=sensor)
-    return create_response(data=updated)
+    return create_response(data=updated, message='Sensor updated')
 
-# @router.post("", status_code=status.HTTP_201_CREATED)
-# async def create_user(
-#         new_user: IUserCreate = Depends(user_deps.user_exists),
-#         current_user: User = Depends(
-#             deps.get_current_user(required_roles=[IRoleEnum.admin])
-#         ),
-# ) -> IPostResponseBase[IUserReadWithGroups]:
-#     """
-#     Creates a new user
-#     Required roles:
-#     - admin
-#     """
-#     user = await crud.user.create_with_role(obj_in=new_user)
-#
-#     if new_user.groups_ids:
-#         for group_id in new_user.groups_ids:
-#             await crud.group.add_user_to_group(user=user, group_id=group_id)
-#
-#     return create_response(data=user)
-#
-# @router.delete("/{user_id}")
-# async def remove_user(
-#         user_id: UUID = Depends(user_deps.is_valid_user_id),
-#         current_user: User = Depends(
-#             deps.get_current_user(required_roles=[IRoleEnum.admin])
-#         ),
-# ) -> IDeleteResponseBase[IUserRead]:
-#     """
-#     Deletes a user by his/her id
-#     Required roles:
-#     - admin
-#     """
-#     if current_user.id == user_id:
-#         raise UserSelfDeleteException()
-#
-#     user = await crud.user.remove(id=user_id)
-#     return create_response(data=user, message="User removed")
-#
-#
-# @router.post("/image")
-# async def upload_my_image(
-#         title: str | None = Body(None),
-#         description: str | None = Body(None),
-#         image_file: UploadFile = File(...),
-#         current_user: User = Depends(deps.get_current_user()),
-#         minio_client: MinioClient = Depends(deps.minio_auth),
-# ) -> IPostResponseBase[IUserRead]:
-#     """
-#     Uploads a user image
-#     """
-#     try:
-#         image_modified = modify_image(BytesIO(image_file.file.read()))
-#         data_file = minio_client.put_object(
-#             file_name=image_file.filename,
-#             file_data=BytesIO(image_modified.file_data),
-#             content_type=image_file.content_type,
-#         )
-#         media = IMediaCreate(
-#             title=title, description=description, path=data_file.file_name
-#         )
-#         user = await crud.user.update_photo(
-#             user=current_user,
-#             image=media,
-#             heigth=image_modified.height,
-#             width=image_modified.width,
-#             file_format=image_modified.file_format,
-#         )
-#         return create_response(data=user)
-#     except Exception as e:
-#         print(e)
-#         return Response("Internal server error", status_code=500)
-#
-#
-# @router.post("/{user_id}/image")
-# async def upload_user_image(
-#         user: User = Depends(user_deps.is_valid_user),
-#         title: str | None = Body(None),
-#         description: str | None = Body(None),
-#         image_file: UploadFile = File(...),
-#         current_user: User = Depends(
-#             deps.get_current_user(required_roles=[IRoleEnum.admin])
-#         ),
-#         minio_client: MinioClient = Depends(deps.minio_auth),
-# ) -> IPostResponseBase[IUserRead]:
-#     """
-#     Uploads a user image by his/her id
-#     Required roles:
-#     - admin
-#     """
-#     try:
-#         image_modified = modify_image(BytesIO(image_file.file.read()))
-#         data_file = minio_client.put_object(
-#             file_name=image_file.filename,
-#             file_data=BytesIO(image_modified.file_data),
-#             content_type=image_file.content_type,
-#         )
-#         media = IMediaCreate(
-#             title=title, description=description, path=data_file.file_name
-#         )
-#         user = await crud.user.update_photo(
-#             user=user,
-#             image=media,
-#             heigth=image_modified.height,
-#             width=image_modified.width,
-#             file_format=image_modified.file_format,
-#         )
-#         return create_response(data=user)
-#     except Exception as e:
-#         print(e)
-#         return Response("Internal server error", status_code=500)
+
+@router.post("", status_code=status.HTTP_201_CREATED)
+async def create_sensor(
+        sensor: schemas.ISensorCreate
+) -> IPostResponseBase[schemas.ISensorRead]:
+    """
+    Creates a new sensor
+    """
+    if sensor.transmitter_id:
+        await checks.transmitter_is_exist(id=sensor.transmitter_id)
+    if sensor.type_id:
+        await checks.sensor_type_is_exist(id=sensor.type_id)
+    if sensor.measurement_type_id:
+        await checks.measurements_type_is_exist(id=sensor.measurement_type_id)
+
+    created = await crud.sensor.create(obj_in=sensor)
+    return create_response(data=created, message='Sensor created')
+
+
+@router.delete("/{id}")
+async def remove_sensor(
+        current_sensor: models.Sensor = Depends(
+            deps.get_sensor_by_id_from_path
+        ),
+) -> IDeleteResponseBase[schemas.ISensorRead]:
+    """
+    Deletes a sensor by id
+    """
+
+    deleted = await crud.sensor.remove(id=current_sensor.id)
+    return create_response(data=deleted, message="Sensor removed")
--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -3,4 +3,4 @@ from app.api.v1.endpoints import (sensor)
 
 
 api_router_v1 = APIRouter()
-api_router_v1.include_router(sensor.router, prefix="/sensor", tags=["sensor"])
+api_router_v1.include_router(sensor.router, prefix="/sensor", tags=["Sensor"])
