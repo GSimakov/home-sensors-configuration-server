6a651f3f5cd634eec58b4000631606ab59887147
gsimakov <yolo73104@gmail.com>
2024-02-02 12:28:35 +0500

    v1 router update

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import (sensor, measurement_type)
+from app.api.v1.endpoints import (sensor, measurement_type, sensor_type)
 
 
 api_router_v1 = APIRouter()
 api_router_v1.include_router(sensor.router, prefix="/sensor", tags=["Sensor"])
 api_router_v1.include_router(measurement_type.router, prefix="/measurement_type", tags=["MeasurementType"])
+api_router_v1.include_router(sensor_type.router, prefix="/sensor_type", tags=["SensorType"])
+
+
